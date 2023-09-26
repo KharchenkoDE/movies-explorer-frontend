2706be7e0aaadfb3d22db305118409c587c7ed14
Dmitry Kharchenko <kharchenkoDE@yandex.ru>
2023-09-26 18:19:14 +0500

    fix: правки после ревью

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,7 +1,4 @@
-export const BASE_URL = 'http://localhost:8000';
-
-
-// export const BASE_URL = 'https://api.kharchenkode.nomoreparties.co';
+export const BASE_URL = 'https://api.kharchenkode.nomoreparties.co';
 
 function checkError(res) {
   if (res.ok) {
